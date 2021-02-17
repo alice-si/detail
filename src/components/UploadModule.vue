680ca36557f81ef519418914efc41e39bdea3f72
Joanna Kang <jfrences11@gmail.com>
2021-02-17 17:21:33 +0000

    feat: create project page markup added & upload module seperated as component

---
+++ src/components/UploadModule.vue
@@ -0,0 +1,393 @@
+<template>
+  <section class='project-data-area'>
+    <div class='file-upload-area'>
+      <h1 class="file-upload-area-title">Your project's data</h1>
+      <h3 class="file-upload-area-subtitle">Please drag & drop one or multiple excel or csv file(s)</h3>
+      <vue-dropzone
+        ref="imgDropZone"
+        id="customdropzone"
+        :options="dropzoneOptions"
+        @vdropzone-complete="afterComplete"
+        @vdropzone-mounted="getFileList"
+      ></vue-dropzone>
+      <div v-if="uploading.length > 0" class="image-div">
+        <div v-for="(file, index) in uploading" :key="index" class="filename-list">
+          <img :src="setImgSrc(index)" class="upload-check"/><p>{{file}}</p><img src="../assets/Trash.svg" class="image" @click="deleteFile(index)"/>
+        </div>
+      </div>  
+      <div class='terms-checkbox-area'>
+        <input type='checkbox' id='terms-checkbox' >
+          <label class='terms-checkbox' for='terms-checkbox'>
+            <div class='terms-checkbox-div' for='terms-checkbox'>
+              <span class='terms-checkbox-v' for='terms-checkbox'>V</span>
+            </div>
+          </label>
+        <h3 class='upload-sub-title'>Please tick the box to agree with our </br> <a href='#'>Data Processing Terms & Conditions</a> </h3>
+      </div>
+    </div>
+    <div class="upload-logo-area">
+      <h3 class="upload-logo-area-subtitle">Connect to your database on: </h3>
+      <img src="../assets/AwsLogo.svg">
+      <img src="../assets/SalesforceLogo.svg">      
+    </div>
+  </section>
+</template>
+
+<script>
+import firebase from 'firebase'
+import vue2Dropzone from 'vue2-dropzone'
+import 'vue2-dropzone/dist/vue2Dropzone.min.css'
+
+export default {
+  name: 'uploadmodule',
+  components: {
+    vueDropzone: vue2Dropzone
+  },
+  data () {
+    return {
+      // imgSrc: require('../assets/UploadCheckIcon_0.svg'),
+      dropzoneOptions: {
+        url: 'https://httpbin.org/post',
+        thumbnailWidth: 150,
+        thumbnailHeight: 150,
+        addRemoveLinks: false,
+        acceptedFiles: '.csv, .xlsx',
+        dictDefaultMessage: `<p class="drag-drop-text">Drag & Drop Files Here</p>`
+      },
+      uploading: [],
+      savedFileList: []
+    }
+  },
+  mounted () {
+    this.hideNavBar()
+  },
+  methods: {
+    hideNavBar () {
+      const navbar = document.getElementById('nav')
+      navbar.style.display = 'none'
+    },
+    getFileList () {
+      const storageRef = firebase.storage().ref()
+      const fileOriginRef = storageRef.child(`files/`)
+      fileOriginRef.listAll()
+        .then((res) => res._delegate.items
+          .forEach((el) => { this.savedFileList.push(el.name) })
+        )
+    },
+    async afterComplete (upload) {
+      if (this.savedFileList.includes(upload.name)) {
+        alert(`[ ${upload.name} ] has already been added in system`)
+        const detailBox = document.getElementsByClassName('dz-preview')
+        for (let i = 0; i < detailBox.length; i++) {
+          detailBox[i].style.display = 'none'
+        }
+      } else {
+        this.isLoading = true
+        try {
+          let file = upload
+          const metadata = {
+            contentType: file.type
+          }
+          const storageRef = firebase.storage().ref()
+          const fileRef = storageRef.child(`files/${file.upload.filename}`)
+          await fileRef.put(file, metadata)
+          this.uploading.push(file.upload.filename)
+          this.$refs.imgDropZone.removeFile(upload)
+        } catch (error) {
+          alert(error)
+        } finally {
+          this.savedFileList = []
+          this.getFileList()
+        }
+      }
+    },
+    deleteFile (index) {
+      const storageRef = firebase.storage().ref()
+      const fileRef = storageRef.child(`files/${this.uploading[index]}`)
+      fileRef.delete()
+        .then(() => {
+          alert(`[ ${this.uploading[index]} ] deleted`)
+          this.uploading.splice(index, 1)
+        }).catch((e) => {
+          alert(e)
+        })
+    },
+    setImgSrc (index) {
+      const imgIndx = index % 3
+      const src = require(`../assets/UploadCheckIcon_${imgIndx}.svg`)
+      return src
+    }
+  }
+}
+
+</script>
+
+<style>
+#upload {
+  display: flex;
+  flex-direction: column;
+  width: 100vw;
+  height: 100vh;
+  background-image: url('../assets/Loginbackground.svg');
+  background-position: center bottom;
+  background-repeat: no-repeat;
+  background-size: 90vw 30vh;
+  align-items: center;
+  padding-top: 12vh;
+}
+
+.project-data-area {
+  display: flex;
+  width: 64.4rem;
+  position: relative;
+  left: 10rem;
+}
+
+.file-upload-area {
+  border-right: 1px solid #DCE2F0;
+  width: 39.6rem;
+  padding-right: 5.8rem;
+  padding-bottom: 3rem;
+}
+
+.file-upload-area-title {
+  margin-top: 5.2rem;
+  font-family: Helvetica;
+  font-size: 2.6rem;
+  color: #8954BA;
+  line-height: 2.4rem;
+  text-align: left;
+}
+
+.file-upload-area-subtitle {
+  font-family: Helvetica;
+  font-size: 1.4rem;
+  color: #686868;
+  text-align: left;
+  margin-top: 1.6rem;
+  margin-bottom : 4.1rem;
+}
+
+.upload-title {
+  font-family: Helvetica;
+  font-size: 2.88rem;
+  color: #8954BA;
+  margin-top : 2rem;
+  }
+
+.upload-sub-title  {
+  font-family: Helvetica;
+  font-size: 1.6rem;
+  color: #686868;
+  text-align: center;
+  margin: 0.2rem 0 0 0;
+  }
+
+.upload-sub-title a {
+  color:#2FB9EF;
+}
+
+.terms-checkbox-area {
+  margin: 2rem 0 0 0;
+  align-items: center;
+  display: flex;
+  justify-content: center;
+}
+
+input[id='terms-checkbox'] {
+  display: none;
+}
+
+input[id='terms-checkbox'] + label {
+  background-color:rgb(255, 255, 255, 0);
+  border: 0.1rem solid #2FB9EF;
+  width: 2rem;
+  height: 2rem;
+  border-radius: 0.2rem;
+  color: rgb(255, 255, 255, 0);
+  margin: 0 1rem 0 0 ;
+}
+
+input[id='terms-checkbox']:checked + label {
+  color: #2FB9EF;
+  width:20px;
+  height:20px;
+}
+
+.terms-checkbox-div {
+  align-items: center;
+  vertical-align: center;
+  width: 1.8rem;
+  height: 1.8rem;
+  margin: 0.2rem 0 0 0;
+}
+
+.terms-checkbox-v {
+  position: relative;
+}
+
+.upload-drop-area {
+  display: flex;
+  justify-content: center;
+  margin-top: 2rem;
+  flex-direction: column;
+  align-items: center;
+}
+
+.drag-drop-text {
+  margin: 0;
+  padding-top: 8rem;
+}
+
+.image-div {
+  width: 30rem;
+  margin-top: 1rem;
+}
+
+.filename-list {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 1rem;
+  border-bottom: 0.5px solid rgb(44, 62, 80, 0.4);
+}
+
+.filename-list p {
+  margin: 0;
+  font-size: 1.6rem;
+  font-family: Helvetica;
+  color: #34495E;
+}
+
+#customdropzone {
+  border: 1px dashed #2FB9EF;
+  background-color: #ffffff;
+  transition: background-color .2s linear;
+  background-image: url('../assets/UploadIcon.svg');
+  background-position: center;
+  background-repeat: no-repeat;
+  font-family: Helvetica;
+  font-size: 12px;
+  color: #686868;
+  width: 300px;
+  padding: 0 ;
+}
+
+.dz-complete {
+  height: 10rem;
+}
+
+.dz-progress {
+  height: 10rem;
+}
+
+.upload-logo-area {
+  display: flex;
+  justify-content: space-evenly;
+  margin: 2rem 0 0 0;
+}
+
+.upload-check {
+  -webkit-animation-name: bounceIn;
+  animation-name: bounceIn;
+  -webkit-animation-duration: 1s;
+  animation-duration: 1s;
+}
+
+@-webkit-keyframes bounceIn {
+  0%, 100%, 20%, 40%, 60%, 80% {
+    -webkit-transition-timing-function: cubic-bezier(0.215, .61, .355, 1);
+    transition-timing-function: cubic-bezier(0.215, .61, .355, 1)
+  }
+  0% {
+    opacity: 0;
+    -webkit-transform: scale3d(.3, .3, .3);
+    transform: scale3d(.3, .3, .3)
+  }
+  20% {
+    -webkit-transform: scale3d(1.1, 1.1, 1.1);
+    transform: scale3d(1.1, 1.1, 1.1)
+  }
+  40% {
+    -webkit-transform: scale3d(.9, .9, .9);
+    transform: scale3d(.9, .9, .9)
+  }
+  60% {
+    opacity: 1;
+    -webkit-transform: scale3d(1.03, 1.03, 1.03);
+    transform: scale3d(1.03, 1.03, 1.03)
+  }
+  80% {
+    -webkit-transform: scale3d(.97, .97, .97);
+    transform: scale3d(.97, .97, .97)
+  }
+  100% {
+    opacity: 1;
+    -webkit-transform: scale3d(1, 1, 1);
+    transform: scale3d(1, 1, 1)
+  }
+}
+@keyframes bounceIn {
+  0%, 100%, 20%, 40%, 60%, 80% {
+    -webkit-transition-timing-function: cubic-bezier(0.215, .61, .355, 1);
+    transition-timing-function: cubic-bezier(0.215, .61, .355, 1)
+  }
+  0% {
+    opacity: 0;
+    -webkit-transform: scale3d(.3, .3, .3);
+    -ms-transform: scale3d(.3, .3, .3);
+    transform: scale3d(.3, .3, .3)
+  }
+  20% {
+    -webkit-transform: scale3d(1.1, 1.1, 1.1);
+    -ms-transform: scale3d(1.1, 1.1, 1.1);
+    transform: scale3d(1.1, 1.1, 1.1)
+  }
+  40% {
+    -webkit-transform: scale3d(.9, .9, .9);
+    -ms-transform: scale3d(.9, .9, .9);
+    transform: scale3d(.9, .9, .9)
+  }
+  60% {
+    opacity: 1;
+    -webkit-transform: scale3d(1.03, 1.03, 1.03);
+    -ms-transform: scale3d(1.03, 1.03, 1.03);
+    transform: scale3d(1.03, 1.03, 1.03)
+  }
+  80% {
+    -webkit-transform: scale3d(.97, .97, .97);
+    -ms-transform: scale3d(.97, .97, .97);
+    transform: scale3d(.97, .97, .97)
+  }
+  100% {
+    opacity: 1;
+    -webkit-transform: scale3d(1, 1, 1);
+    -ms-transform: scale3d(1, 1, 1);
+    transform: scale3d(1, 1, 1)
+  }
+}
+
+.upload-logo-area {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+  margin-top: 5.2rem;
+  padding-left: 5.6rem;
+  padding-bottom: 3rem;
+  width: 24.3rem;
+}
+
+.upload-logo-area-subtitle {
+  font-family: Helvetica;
+  font-size: 1.4rem;
+  color: #686868;
+  margin-top: 4rem;
+  margin-bottom: 0;
+}
+
+.upload-logo-area img {
+  margin-top : 4.1rem;
+}
+
+</style>
--- src/views/CreateProject.vue
+++ src/views/CreateProject.vue
@@ -1,33 +1,41 @@
 <template>
-  <div class="creat-project">
-    <main id="creat-project">
-      <section class="creat-project-form-area">
-        <img src="../assets/alice_logo.png" width="91rem"/>
-        <h1 class="creat-project-title">Create your project</h1>
-        <form>
-          <h1>Your company's name</h1>
-          <input type="text" class="companyname-text" placeholder="eg Vodafone Foundation">
-          <h1>Your project's name</h1>
-          <input type="text" class="projectname-text" placeholder="eg Instant Network Schools - INS">
-          <h1>Your project's name</h1>
+  <main id="creat-project">
+    <section class="creat-project-form-area">
+      <img src="../assets/alice_logo.png" width="91rem"/>
+      <h1 class="creat-project-title">Create your project</h1>
+      <form>
+        <h1>Your company's name</h1>
+        <input type="text" class="companyname-text" placeholder="eg Vodafone Foundation">
+        <h1>Your project's name</h1>
+        <input type="text" class="projectname-text" placeholder="eg Instant Network Schools - INS">
+        <h1>Your project’s impact objective(s)</h1>
+        <div class="project-objective-input">
           <input type="text" class="objective-text" placeholder="eg How many lessons use INS">
-          <input type="button" value="+" class="add-button"></input>
-        </form>
-      </section>
-    </main>
-    <button @click="logOut">Logout</button>
-  </div>
+          <input type="button" value="+" class="add-button-1"/>
+        </div>
+        <div class="project-objective-input">
+          <input type="text" class="objective-text" placeholder="eg How many students have ICT skills ">
+          <input type="button" value="+" class="add-button-2"/>
+        </div>
+      </form>
+    <button class="project-create-button">Create</button>
+    <button @click="logOut" class="logout-button">Logout</button>
+    </section>
+    <upload-module></upload-module>
+    <button class="file-upload-button">Upload</button>
+  </main>
 </template>
 
 <script>
 import router from '../router'
 import { store } from '../store/store'
+import UploadModule from '../components/UploadModule.vue'
 
 export default {
   name: 'creat-project',
-  // components: {
-
-  // },
+  components: {
+    UploadModule
+  },
   // data () {
   //   return {
 
@@ -69,23 +77,26 @@ button:focus {
 #creat-project {
   display: flex;
   flex-direction: column;
-  width: 100vw;
-  height: 100vh;
+  width: 100%;
+  /* height: 100vh; */
+  background-image: url('../assets/Loginbackground.svg');
   background-position: center bottom;
   background-repeat: no-repeat;
   background-size: 90vw 30vh;
   align-items: center;
   vertical-align: center;
+  overflow-y: auto;
+  overflow-x: hidden;
+  padding: 6.5rem 0 30rem 0;
 }
 
-.creat-project-form-area {
+/* .creat-project-form-area {
   position: absolute;
   top: 6%;
-}
+} */
 
 .companyname-text,
-.projectname-text,
-.objective-text  {
+.projectname-text {
   margin-top : 1.2rem;
   margin-bottom : 5rem;
   border: none;
@@ -107,7 +118,7 @@ button:focus {
   flex-direction: column;
   background: rgba(255,255,255,0.00);
   margin-top: 3.5rem;
-  align-items: center;
+  align-items: left;
   width: 100%;
   font-family: Helvetica;
   font-size: 1.68rem;
@@ -120,29 +131,15 @@ button:focus {
   font-size: 2.6rem;
   color: #8954BA;
   line-height: 2.4rem;
+  text-align: left;
 }
 
 .creat-project-text{
   border: none;
 }
 
-#creat-project .sign-up {
-  float: inline-end;
-  background: #5D38DB;
-  box-shadow: 0 14px 42px 0 rgba(52,77,178,0.34);
-  border-radius: 2.4px;
-  color: #ffffff;
-  height: 5.5rem;
-  width: 12rem;
-  position: absolute;
-  top: 49.5%;
-  left: 89%;
-  border: none;
-  font-family: .SFNS-Regular;
-  font-size: 1.68rem;
-}
-
-#creat-project .add-button {
+#creat-project .add-button-1,
+#creat-project .add-button-2 {
   background-color: #ffffff;
   border: none;
   border-radius: 60px;
@@ -151,96 +148,102 @@ button:focus {
   height: 5.4rem;
   color: #8954BA;
   box-shadow: 0 7px 20px 0 rgba(159,168,214,0.59);
-}
-
-.signing-up-text {
-  font-size: 1.68rem;
-  color: #8954BA;
-  margin: 9.4rem;
-}
-
-.signing-up-text a {
-  display: inline;
-  text-decoration: underline;
-  font-family: Helvetica;
-  font-size: 1.68rem;
-  color: #8954BA;
-}
-
-input[id='rememberme-checkbox'] {
-  display: none;
-}
-
-input[id='rememberme-checkbox'] + label {
-  background-color:rgb(255, 255, 255, 0);
-  border: 0.1rem solid #8954BA;
-  width: 2rem;
-  height: 2rem;
-  border-radius: 0.2rem;
-  color: rgb(255, 255, 255, 0);
-  margin: 0 1rem 0 0 ;
   position: relative;
-  top: 0.3rem;
+  right: -3rem;
 }
 
-input[id='rememberme-checkbox']:checked + label { 
-  color: #8954BA;
-  width: 2rem;
-  height: 2rem;
-}
-
-.rememberme-checkbox-div {
-  width: 2rem;
-  height: 2rem;
-  margin: 0;
-  padding: 0;
-}
-
-.rememberme-checkbox-v {
-  font-size: 1.2rem;
-  padding: 0;
-  margin: 0;
-  position: relative;
-  top: -0.35rem;
-}
-
-.rememberme-area {
-  font-family: Helvetica;
-  font-size: 1.68rem;
-  color: #8954BA;
-  margin-top: 1.5rem;
+.project-objective-input {
   display: flex;
-  padding: 0 1.9rem;
+  align-items: center;
+  margin-top: 2rem;
 }
 
-.rememberme-area h3 {
-  display: inline;
-  margin: 0;
-}
-
-.remember-me {
-  padding: 0;
-  margin: 0;
-}
-
-.remember-me a {
-  text-decoration: underline;
-  margin-left: 0.5rem;
-  color: #8954BA;  
-}
-
-.sign-in-with-google {
-  width: 50.7rem;
-  height: 2rem;
-  background-color : white;
+.objective-text {
   border: none;
-  font-size: 1.68rem;
-  text-align: left;
-  padding: 1.6rem 2rem;
-  color:  #686868;
-  display: flex;
-  justify-content: space-between;
-  box-shadow: 0 0.5rem 3rem 0 rgba(52,77,178,0.34);
+  height: 4.6rem;
+  width: 39.8rem;
+  padding: 1.8rem 2.3rem;
 }
 
+.creat-project-form-area .logout-button {
+  /* margin-right: 2rem; */
+  border: none;
+  padding: 1rem 1.5rem;
+  margin: 0 0.5rem;
+  border-radius: 0.2rem;
+  transition: all 0.3s ease;
+  font-size: 1.68rem;
+  /* position: absolute;
+  top: 5rem;
+  right:2rem; */
+}
+
+.project-create-button {
+  border: none;
+  padding: 1rem 1.5rem;
+  margin: 0 0.5rem;
+  border-radius: 0.2rem;
+  transition: all 0.3s ease;
+  background-color: #5D38DB;
+  color: #ffffff;
+  font-size: 1.68rem;
+}
+
+.creat-project-form-area .logout-button:hover {
+  border: none;
+  padding: 1rem 1.5rem;
+  border-radius: 0.2rem;
+  background-color: #5D38DB;
+  color: #ffffff;
+  font-size: 1.88rem;
+}
+
+.project-create-button:hover {
+  border: none;
+  padding: 1rem 1.5rem;
+  border-radius: 0.2rem;
+  background-color: #efefef;
+  color: #000000;
+  font-size: 1.88rem;
+}
+
+.project-create-button,
+.creat-project-form-area .logout-button {
+  margin-top: 3rem;
+  height: 5.5rem;
+}
+
+.creat-project-form-area .logout-button {
+  position: relative;
+  left: -3rem;
+}
+
+.project-create-button {
+  position: relative;
+  right: -22rem;
+  /* height: 5.5rem; */
+  width: 12rem;
+}
+
+.file-upload-button {
+  border: none;
+  padding: 1rem 1.5rem;
+  margin: 0 0.5rem;
+  border-radius: 0.2rem;
+  transition: all 0.3s ease;
+  font-size: 1.68rem;
+  position: relative;
+  right: -18rem;
+  width: 12rem;
+  height: 5.5rem;
+}
+
+.file-upload-button:hover {
+  border: none;
+  padding: 1rem 1.5rem;
+  border-radius: 0.2rem;
+  background-color: #5D38DB;
+  color: #ffffff;
+  font-size: 1.88rem;
+}
 </style>
